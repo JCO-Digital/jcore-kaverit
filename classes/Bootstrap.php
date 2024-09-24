7a0dd5ed94256e10d5d38e06e13b054871ea2be0
Johan Österberg <johan.osterberg@jco.fi>
2024-09-24 11:40:07 +0300

    feat: Set namespace. ✨

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -1,10 +1,12 @@
 <?php // phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
 
-namespace Jcore\Template;
+namespace Jcore\Kaverit;
 
 use Jcore\Ydin\BootstrapInterface;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if ( is_file( __DIR__ . '/../vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/../vendor/autoload.php';
+}
 
 /**
  * The bootstrap class, should be used by all dependencies.
